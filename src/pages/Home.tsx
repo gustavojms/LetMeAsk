c0bdfc843069d92e2d40d889c3d14ce9da5e543a
Gustavo Jose <gustavo.joset0@hotmail.com>
2022-09-01 19:40:00 -0300

    Criado autenticação Google utilizando firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,20 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss'
 import { Button } from "../components/Button";
 import { useNavigate } from "react-router-dom";
+import { firebase, auth } from "../services/firebase"
 
 export function Home() {
 
     const navigate = useNavigate();
 
-    function navigateToNewRoom() {
-        navigate("/rooms/new")
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result)
+            navigate("/rooms/new")
+        })
+
     }
 
     return (
@@ -24,7 +31,7 @@ export function Home() {
           <main>
             <div className="main-content">
                 <img src={ logoImg } alt="Letmeask"/>
-                <button  onClick={ navigateToNewRoom } className="create-room">
+                <button  onClick={ handleCreateRoom } className="create-room">
                     <img src={ googleIconImg } alt="Logo do Google"/>
                     Crie sua sala com o google
                 </button>
